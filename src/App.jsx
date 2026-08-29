5dd4a2d8fade1a8bc6e8eb50dea5835f237bf88f
joaopmag <joaopmag@gmail.com>
2026-08-29 02:09:17 +0100

    Update App.jsx
    foto scouting

--- src/App.jsx
+++ src/App.jsx
@@ -23063,7 +23063,7 @@ function ScoutSheetPage({ player: x, onBack, onEdit, onShare, onPrint }) {
         </table>
         {x.photo && (
           <img src={x.photo} alt="Fotografia" style={{
-            position: 'absolute', top: 38, right: 0, width: 100, aspectRatio: '3 / 4', objectFit: 'cover',
+            position: 'absolute', top: 38, right: 30, width: 118, aspectRatio: '3 / 4', objectFit: 'cover',
             borderRadius: 10, border: `1px solid ${T.line}`, background: '#000',
           }} />
         )}
@@ -23097,8 +23097,8 @@ function ScoutSheetPage({ player: x, onBack, onEdit, onShare, onPrint }) {
           <div style={{ fontSize: 13, color: T.warn, fontWeight: 600, marginTop: 14 }}>Média dos 4 pilares: {avg}</div>
         )}
 
-        <div style={{ position: 'absolute', top: 38, right: 0 }}>
-          <QuadroPosicaoJogador position={x.position} secondaryPosition={x.secondaryPosition} largura={90} />
+        <div style={{ position: 'absolute', top: 38, right: 30 }}>
+          <QuadroPosicaoJogador position={x.position} secondaryPosition={x.secondaryPosition} largura={106} />
         </div>
       </div>
 
